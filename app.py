52b2e49c6cdc6ddcb3b6b55188f218a0b7df429c
Olli Partanen <olli.partanen@edu.lapinamk.com>
2025-04-05 12:04:26 +0300

    Base code, testmethods and flask import. Requirements create update

---
+++ app.py
@@ -0,0 +1,19 @@
+from flask import Flask, request;
+
+app = Flask(__name__)
+
+# Empty route Hello world test
+@app.route("/")
+def hello_world():
+    return "<p>Hello ja Terve from cc backend</p>"
+
+# POST-method for data endpoint. Print name-info presented in the request body
+@app.route('/data', methods=['POST'])
+def get_data():
+    data = request.json
+    print(data["name"])
+    return {"message": "Data received", "data": data}
+
+if __name__ == '__main__':
+    #app.run()
+    app.run(host="0.0.0.0", port="5000", debug=True)
